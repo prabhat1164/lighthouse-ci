dc1b9719b266e70b4c35af0e01b5eda2b54561c6
Prabhat Kumar <Prabhat.11911968@lpu.co.in>
2022-12-16 15:19:08 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is am about MyComponents
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nesciunt minus perspiciatis qui fuga, nihil voluptates, reprehenderit, quisquam nulla laborum ducimus pariatur omnis ad autem illo id velit tenetur nobis sapiente eligendi dolore esse.
+        </p>
+        <p>
+          Hello this is about 2
+          Changes 2
+        </p>
+    </div>
+  )
+}
